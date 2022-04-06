6a89708207351718f60b43ef4d4a99de51f22df9
Max Bocharnikov <bocharnikov3009@gmail.com>
2022-04-06 18:00:01 +0700

    init FE project

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import S from './App.styled';
+
+const App = () => {
+  return (
+    <div>
+      <S.Title>Hello from React</S.Title>
+    </div>
+  );
+};
+
+export default App;
