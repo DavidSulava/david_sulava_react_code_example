bb0f7615e9942123ad7edcd2a3cee83d9f02ea0d
ANTON KROKOVNY <kronya@themsteam.com>
2022-04-29 17:07:09 +0700

    CRUD Configuration

---
+++ DesignGear.Common/Exceptions/OperationErrorException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DesignGear.Common.Exceptions
+{
+    [Serializable]
+    public class OperationErrorException : BaseException
+    {
+        public OperationErrorException(int code)
+            : base(code)
+        {
+        }
+
+        public OperationErrorException(int code, string message)
+            : base(code, message)
+        {
+        }
+
+        public OperationErrorException(int code, string message, Exception innerException)
+            : base(code, message, innerException)
+        {
+        }
+
+        protected OperationErrorException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
