096e8d1d6c0577d3d58ce59780de2d443aa14ada
Konstantin Tsymbalist <kon.tsimbalist@gmail.com>
2022-05-13 20:39:57 +0800

    Adding cors

--- Services/Contractor/DesignGear.Contractor.Api/Program.cs
+++ Services/Contractor/DesignGear.Contractor.Api/Program.cs
@@ -49,6 +49,22 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins(
+            "http://localhost:3000",
+            "https://localhost:3000",
+            "http://localhost:3000/",
+            "https://localhost:3000/");
+
+
+        builder.WithExposedHeaders("Content-Disposition");
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+        builder.AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -58,6 +74,8 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseMiddleware<JwtMiddleware>();
 
