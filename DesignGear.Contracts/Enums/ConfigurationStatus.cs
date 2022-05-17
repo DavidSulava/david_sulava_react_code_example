b3974d1f03ef6ad7bf241a939f8170164c90a653
Konstantin Tsymbalist <kon.tsimbalist@gmail.com>
2022-05-17 16:01:11 +0800

    Commenting

--- DesignGear.Contracts/Enums/ConfigurationStatus.cs
+++ DesignGear.Contracts/Enums/ConfigurationStatus.cs
@@ -8,8 +8,10 @@ namespace DesignGear.Contracts.Enums {
     public enum ConfigurationStatus {
         InQueue,
         InProcess,
+        /*Ограничить кол-во запросов для заявок на конфигурацию с данным статусом (или прекратить делать запросы совсем или увеличивать интревал между запросами)*/
         ServiceUnavailableError,
         IncorrectRequestError,
+        InvalidConfiguration,
         Ready
     }
 }
